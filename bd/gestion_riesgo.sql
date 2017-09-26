ef2104b8d42d3616855aed650d4c4b81f22d7ca0
ender18 <enderortega@outlook.com>
2017-09-26 07:46:02 -0500

    bd

--- bd/gestion_riesgo.sql
+++ bd/gestion_riesgo.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-09-2017 a las 05:38:48
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.6.21
+-- Tiempo de generación: 26-09-2017 a las 14:45:23
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 5.5.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -39,9 +39,9 @@ CREATE TABLE `proyecto` (
 
 INSERT INTO `proyecto` (`id_proyecto`, `nombre`, `gerente`, `fecha_registro`) VALUES
 (7, '989', '898989', '2017-09-14 22:18:51'),
-(8, 'i898', '98989', '2017-09-14 22:18:56'),
 (15, 'vectores', 'ender', '2017-09-16 22:43:50'),
-(17, 'base de datos', 'omar', '2017-09-17 07:18:47');
+(18, 'Cinera', 'Ender', '2017-09-19 11:47:44'),
+(19, 'integrador', 'gino', '2017-09-26 12:41:59');
 
 -- --------------------------------------------------------
 
@@ -57,18 +57,25 @@ CREATE TABLE `riesgo` (
   `efectos` text NOT NULL,
   `como_impacta` text NOT NULL,
   `impacto` int(11) NOT NULL,
-  `probabilidad` int(11) NOT NULL
+  `probabilidad` int(11) NOT NULL,
+  `acciones` text NOT NULL,
+  `responsable` text NOT NULL,
+  `cronograma` text NOT NULL,
+  `indicador` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `riesgo`
 --
 
-INSERT INTO `riesgo` (`id_proyecto`, `id_riesgo`, `riesgo`, `causas`, `efectos`, `como_impacta`, `impacto`, `probabilidad`) VALUES
-(7, 7, 'No saber programar', '1. ser idiota<br />\r\n2. salir del sena', '1. No tener trabajo<br />\r\n2. No se sabe quien es mas estupido entre todos los involucrados', 'Muy negativo ya que por medio de este no se puede medir absolutamente nada', 2, 5),
-(7, 8, 'Riesgo2', '1 causa<br />\r\n2 causa', '1<br />\r\n2', 'como impacta', 2, 3),
-(7, 9, 'esto es un riesgo', '909', '09090', '0909', 2, 2),
-(7, 10, 'iuiu', 'iuiu', 'iuiui', 'uiuiui', 3, 4);
+INSERT INTO `riesgo` (`id_proyecto`, `id_riesgo`, `riesgo`, `causas`, `efectos`, `como_impacta`, `impacto`, `probabilidad`, `acciones`, `responsable`, `cronograma`, `indicador`) VALUES
+(7, 7, 'No saber programar', '1. ser idiota<br />\r\n2. salir del sena', '1. No tener trabajo<br />\r\n2. No se sabe quien es mas estupido entre todos los involucrados', 'Muy negativo ya que por medio de este no se puede medir absolutamente nada', 2, 5, '', '', '', ''),
+(7, 8, 'Riesgo2', '1 causa<br />\r\n2 causa', '1<br />\r\n2', 'como impacta', 2, 3, '', '', '', ''),
+(7, 9, 'esto es un riesgo', '909', '09090', '0909', 2, 2, '', '', '', ''),
+(7, 10, 'iuiu', 'iuiu', 'iuiui', 'uiuiui', 3, 4, '', '', '', ''),
+(18, 11, 'Perdida de dinero', 'Gabriel', 'No realizaciÃ³n', 'Suspende el proyecto', 5, 1, 'accion1', 'responsable1', 'cronograma1', 'indicador1'),
+(18, 12, 'DaÃ±os irreparables', 'Humedad de la ciudad', 'Servidores quemados', 'Atrasa el desarrollo del producto', 5, 5, '', '', '', ''),
+(19, 13, 'Riesgo1', 'causa1<br />\r\ncausa2', 'efecto1<br />\r\nefecto2', 'negativamente', 5, 2, 'accion1', 'responsable1', 'cronograma1', 'indicador');
 
 -- --------------------------------------------------------
 
@@ -86,6 +93,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`username`, `password`) VALUES
+('1', '356a192b7913b04c54574d18c28d46e6395428ab'),
 ('omar', '7c4a8d09ca3762af61e59520943dc26494f8941b');
 
 --
@@ -119,12 +127,12 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `proyecto`
 --
 ALTER TABLE `proyecto`
-  MODIFY `id_proyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_proyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT de la tabla `riesgo`
 --
 ALTER TABLE `riesgo`
-  MODIFY `id_riesgo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_riesgo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- Restricciones para tablas volcadas
 --
